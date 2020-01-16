601e581cca2a826299e5122c87dc43251bed551f
Robin Litteaut <litteautr@gmail.com>
2020-01-16 14:53:15 +0100

    First test : convert input number into string

---
+++ src/main/java/fr/rli/foobarqix/FooBarQixConverter.java
@@ -0,0 +1,8 @@
+package fr.rli.foobarqix;
+
+public class FooBarQixConverter {
+
+    public String convert(int inputNumber) {
+        return String.valueOf(inputNumber);
+    }
+}
---
+++ src/test/java/fr/rli/foobarqix/FooBarQixConverterTest.java
@@ -0,0 +1,23 @@
+package fr.rli.foobarqix;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FooBarQixConverterTest {
+
+    private static FooBarQixConverter fbqConverter;
+
+    @BeforeAll
+    static void setUp() {
+        fbqConverter = new FooBarQixConverter();
+    }
+
+    @Test
+    public void should_return_input_number_as_string() {
+        assertEquals("1",fbqConverter.convert(1));
+    }
+
+}
