f2c11318437d8d089a48b504f025cbbab3c240b8
Loïc GEORGES <loic.georges-extern@renault.com>
2020-01-17 22:46:41 +0100

    Refactoring + renaming

--- src/main/java/fr/rli/foobarqix/FooBarQixConverter.java
+++ src/main/java/fr/rli/foobarqix/FooBarQixConverter.java
@@ -6,43 +6,45 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.lang.Character.getNumericValue;
+import static java.util.Map.entry;
 
 public class FooBarQixConverter {
 
-    private static final Map<Integer, String> DIVISION_CONVERTER_RULES;
-    private static final Map<Integer, String> CONTAIN_CONVERTER_RULES = Map.of(3, "Foo", 5, "Bar", 7, "Qix");
+    private static final Map<Integer, String> DIVISION_RULES;
+    private static final Map<Integer, String> CONTAIN_RULES;
 
     static {
         Map<Integer, String> divisionConverterMap = new LinkedHashMap<>();
         divisionConverterMap.put(3, "Foo");
         divisionConverterMap.put(5, "Bar");
+        DIVISION_RULES = Collections.unmodifiableMap(divisionConverterMap);
 
-        DIVISION_CONVERTER_RULES = Collections.unmodifiableMap(divisionConverterMap);
+        CONTAIN_RULES = Map.ofEntries(
+                entry(3, "Foo"),
+                entry(5, "Bar"),
+                entry(7, "Qix"));
     }
 
     public String convert(int inputNumber) {
-        StringBuilder convertedInput = new StringBuilder();
+        StringBuilder convertedInput = new StringBuilder()
+                .append(divisibleConversion(inputNumber))
+                .append(containingConversion(inputNumber));
 
-        convertedInput.append(divisibleConversion(inputNumber));
-        convertedInput.append(containConversion(inputNumber));
-
-        return convertedInput.length() == 0 ? String.valueOf(inputNumber) : convertedInput.toString();
+        return String.valueOf(convertedInput.length() == 0 ? inputNumber : convertedInput);
     }
 
     private String divisibleConversion(int inputNumber) {
-        return DIVISION_CONVERTER_RULES.keySet()
-                .stream()
+        return DIVISION_RULES.keySet().stream()
                 .filter(divisor -> inputNumber % divisor == 0)
-                .map(DIVISION_CONVERTER_RULES::get)
+                .map(DIVISION_RULES::get)
                 .collect(Collectors.joining());
     }
 
-    private String containConversion(int inputNumber) {
-        String numberToString = String.valueOf(inputNumber);
-
-        return numberToString.chars()
-                .filter(x -> CONTAIN_CONVERTER_RULES.containsKey(getNumericValue(x)))
-                .mapToObj(x -> CONTAIN_CONVERTER_RULES.get(getNumericValue(x)))
+    private String containingConversion(int inputNumber) {
+        return String.valueOf(inputNumber)
+                .chars()
+                .mapToObj(x -> CONTAIN_RULES.getOrDefault(getNumericValue(x), ""))
                 .collect(Collectors.joining());
     }
+
 }
--- src/test/java/fr/rli/foobarqix/FooBarQixConverterTest.java
+++ src/test/java/fr/rli/foobarqix/FooBarQixConverterTest.java
@@ -1,7 +1,7 @@
 package fr.rli.foobarqix;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,14 +10,14 @@ import java.util.stream.Stream;
 
 public class FooBarQixConverterTest {
 
-    private static FooBarQixConverter fbqConverter;
+    private FooBarQixConverter fooBarQixConverter;
 
-    @BeforeAll
-    static void setUp() {
-        fbqConverter = new FooBarQixConverter();
+    @BeforeEach
+    void setUp() {
+        fooBarQixConverter = new FooBarQixConverter();
     }
 
-    private static Stream<Arguments> provide_number_for_foobarqix_conversion() {
+    private static Stream<Arguments> provide_numbers_for_foobarqix_conversion() {
         return Stream.of(
                 Arguments.of(1, "1"), // No specific rule for conversion
                 Arguments.of(9, "Foo"), // Divisible by 3
@@ -34,8 +34,8 @@ public class FooBarQixConverterTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provide_number_for_foobarqix_conversion")
-    public void should_apply_foobarqix_conversion_for_input_number(int input, String expected) {
-        Assertions.assertEquals(expected, fbqConverter.convert(input));
+    @MethodSource("provide_numbers_for_foobarqix_conversion")
+    public void should_apply_foobarqix_conversion_for_input_number(int number, String expected) {
+        Assertions.assertEquals(expected, fooBarQixConverter.convert(number));
     }
 }
