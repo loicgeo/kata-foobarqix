5b360b6352aa9801fe448189fe4c621f9c9191ac
Loïc GEORGES <loic.georges-extern@renault.com>
2020-01-17 20:50:55 +0100

    add Main comment to specify no arg needed and explain what program does

--- src/main/java/fr/rli/foobarqix/Main.java
+++ src/main/java/fr/rli/foobarqix/Main.java
@@ -4,6 +4,12 @@ import java.util.stream.IntStream;
 
 public class Main {
 
+    /**
+     * Convert and print conversion result for range of numbers, from 1 to 100.
+     *
+     * @param args no arg needed here
+     * @see README.md for conversion rules.
+     */
     public static void main(String[] args) {
         FooBarQixConverter fooBarQixConverter = new FooBarQixConverter();
 
